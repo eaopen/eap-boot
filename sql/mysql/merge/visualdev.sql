65b4fcc7334c996b0ffdf0eaaf6f72a42b518856
dev <dev@dev.com>
2024-01-19 12:50:04 +0800

    商品 库存1-3，fix 查询/排序

--- sql/mysql/merge/visualdev.sql
+++ sql/mysql/merge/visualdev.sql
@@ -1,5 +1,9 @@
 -- 增加国际化字段 hasI18n(F_HasI18n)
+ALTER TABLE `base_visualdev`
+    ADD COLUMN `F_HasI18n` int NULL COMMENT '启用国际化' AFTER `F_WebType`;
 
+ALTER TABLE `base_visualdev_release`
+    ADD COLUMN `F_HasI18n` int NULL COMMENT '启用国际化' AFTER `F_WebType`;
 
 -- merge jeecg/jnpf data
 
