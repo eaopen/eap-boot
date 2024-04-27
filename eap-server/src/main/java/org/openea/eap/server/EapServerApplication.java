74b1ee029512e3377d66deea82772aa6fd4d27e7
dev <dev@dev.com>
2024-04-28 02:47:18 +0800

    2.6.0 snapshot

--- eap-server/src/main/java/org/openea/eap/server/EapServerApplication.java
+++ eap-server/src/main/java/org/openea/eap/server/EapServerApplication.java
@@ -25,18 +25,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class EapServerApplication {
 
     public static void main(String[] args) {
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
 
         SpringApplication.run(EapServerApplication.class, args);
 //        new SpringApplicationBuilder(EapServerApplication.class)
 //                .applicationStartup(new BufferingApplicationStartup(20480))
 //                .run(args);
 
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
     }
 
 }
