e98478e0d6244dbb3c6c5dd21e9beb9778238838
dev <dev@dev.com>
2024-08-25 13:46:43 +0800

    fix load EapQuartzAutoConfiguration

--- eap-server/src/main/java/org/openea/eap/server/EapServerApplication.java
+++ eap-server/src/main/java/org/openea/eap/server/EapServerApplication.java
@@ -1,7 +1,9 @@
 package org.openea.eap.server;
 
+import org.openea.eap.framework.quartz.config.EapQuartzAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * 项目的启动类
@@ -11,8 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
  *
  */
+
 @SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${eap.info.base-package}
-@SpringBootApplication(scanBasePackages = {"${eap.info.base-package}.server", "${eap.info.base-package}.module"})
+@SpringBootApplication(scanBasePackages = {
+        "${eap.info.base-package}.server",
+        "${eap.info.base-package}.module",
+        "${eap.info.base-package}.framework.quartz"})
+@Import(EapQuartzAutoConfiguration.class)  // load in boot only
 public class EapServerApplication {
 
     public static void main(String[] args) {
