90f9112ad12935f9de04febb74053607ba961675
bobo <bobolau@live.com>
2024-12-23 11:15:36 +0800

    update banner

--- eap-server/src/main/java/org/openea/eap/server/controller/DefaultController.java
+++ eap-server/src/main/java/org/openea/eap/server/controller/DefaultController.java
@@ -14,54 +14,24 @@ import static org.openea.eap.framework.common.exception.enums.GlobalErrorCodeCon
 @RestController
 public class DefaultController {
 
-    @RequestMapping("/admin-api/bpm/**")
-    public CommonResult<Boolean> bpm404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[工作流模块 eap-module-bpm - 已禁用][参考 https://doc.iocoder.cn/bpm/ 开启]");
-    }
+//    @RequestMapping("/admin-api/bpm/**")
+//    public CommonResult<Boolean> bpm404() {
+//        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
+//                "[工作流模块 eap-module-bpm - 已禁用]");
+//    }
 
-    @RequestMapping("/admin-api/mp/**")
-    public CommonResult<Boolean> mp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[微信公众号 eap-module-mp - 已禁用][参考 https://doc.iocoder.cn/mp/build/ 开启]");
-    }
+//    @RequestMapping(value = {"/admin-api/product/**", // 商品中心
+//            "/admin-api/trade/**", // 交易中心
+//            "/admin-api/promotion/**"})  // 营销中心
+//    public CommonResult<Boolean> mall404() {
+//        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
+//                "[商城系统 eap-module-mall - 已禁用]");
+//    }
 
-    @RequestMapping(value = {"/admin-api/product/**", // 商品中心
-            "/admin-api/trade/**", // 交易中心
-            "/admin-api/promotion/**"})  // 营销中心
-    public CommonResult<Boolean> mall404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[商城系统 eap-module-mall - 已禁用][参考 https://doc.iocoder.cn/mall/build/ 开启]");
-    }
-
-    @RequestMapping("/admin-api/erp/**")
-    public CommonResult<Boolean> erp404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[ERP 模块 eap-module-erp - 已禁用][参考 https://doc.iocoder.cn/erp/build/ 开启]");
-    }
-
-    @RequestMapping("/admin-api/crm/**")
-    public CommonResult<Boolean> crm404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[CRM 模块 eap-module-crm - 已禁用][参考 https://doc.iocoder.cn/crm/build/ 开启]");
-    }
-
-    @RequestMapping(value = {"/admin-api/report/**"})
-    public CommonResult<Boolean> report404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[报表模块 eap-module-report - 已禁用][参考 https://doc.iocoder.cn/report/ 开启]");
-    }
-
-    @RequestMapping(value = {"/admin-api/pay/**"})
-    public CommonResult<Boolean> pay404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[支付模块 eap-module-pay - 已禁用][参考 https://doc.iocoder.cn/pay/build/ 开启]");
-    }
-
-    @RequestMapping(value = {"/admin-api/ai/**"})
-    public CommonResult<Boolean> ai404() {
-        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
-                "[AI 大模型 eap-module-ai - 已禁用][参考 https://doc.iocoder.cn/ai/build/ 开启]");
-    }
+//    @RequestMapping(value = {"/admin-api/ai/**"})
+//    public CommonResult<Boolean> ai404() {
+//        return CommonResult.error(NOT_IMPLEMENTED.getCode(),
+//                "[AI 大模型 eap-module-ai - 已禁用]");
+//    }
 
 }
