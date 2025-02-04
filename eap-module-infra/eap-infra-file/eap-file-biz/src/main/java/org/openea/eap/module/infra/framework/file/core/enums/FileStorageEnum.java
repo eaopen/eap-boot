f94a305aa55e4873177b49f82c986df017213d40
bobo <bobolau@live.com>
2025-02-04 19:54:00 +0800

    分拆S3和Minio

--- eap-module-infra/eap-infra-file/eap-file-biz/src/main/java/org/openea/eap/module/infra/framework/file/core/enums/FileStorageEnum.java
+++ eap-module-infra/eap-infra-file/eap-file-biz/src/main/java/org/openea/eap/module/infra/framework/file/core/enums/FileStorageEnum.java
@@ -10,6 +10,7 @@ import org.openea.eap.module.infra.framework.file.core.client.ftp.FtpFileClientC
 import org.openea.eap.module.infra.framework.file.core.client.local.LocalFileClient;
 import org.openea.eap.module.infra.framework.file.core.client.local.LocalFileClientConfig;
 import org.openea.eap.module.infra.framework.file.core.client.s3.MinioFileClient;
+import org.openea.eap.module.infra.framework.file.core.client.s3.S3FileClient;
 import org.openea.eap.module.infra.framework.file.core.client.s3.S3FileClientConfig;
 import org.openea.eap.module.infra.framework.file.core.client.sftp.SftpFileClient;
 import org.openea.eap.module.infra.framework.file.core.client.sftp.SftpFileClientConfig;
@@ -30,7 +31,8 @@ public enum FileStorageEnum {
     FTP(11, FtpFileClientConfig.class, FtpFileClient.class),
     SFTP(12, SftpFileClientConfig.class, SftpFileClient.class),
 
-    S3(20, S3FileClientConfig.class, MinioFileClient.class),
+    S3(20, S3FileClientConfig.class, S3FileClient.class),
+    MINIO(21, S3FileClientConfig.class, MinioFileClient.class),
     ;
 
     /**
