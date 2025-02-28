349543f8eb4feea6e73ae0e8cc7ebce35bca7610
bobo <bobolau@live.com>
2025-02-28 12:22:55 +0800

    优化eap-common-share-api

--- eap-module-system/eap-module-system-api/src/main/java/org/openea/eap/module/system/enums/ApiConstants.java
+++ eap-module-system/eap-module-system-api/src/main/java/org/openea/eap/module/system/enums/ApiConstants.java
@@ -19,4 +19,6 @@ public class ApiConstants {
 
     public static final String VERSION = "1.0.0";
 
+    // same as AdminUserApi.PREFIX
+    public static final String ADMIN_USER_PREFIX = PREFIX + "/user";
 }
