9b37db8926965c8e09225e3e91bddac44ee751b3
dev9-bb <openeadev9@gmail.com>
2025-10-24 09:09:59 +0800

    移除未使用的AbstractEngineConfiguration.java文件

--- script/sql/mysql/mysql.sql
+++
@@ -1,9 +0,0 @@
--- 语法
--- create database #databaseName;
--- create user '#userName'@'#host' identified by '#passWord';
--- grant #auth on #databaseName.#table to '#userName'@'#host';
-
-
-create database eap-db;
-create user 'eap'@'%' identified by 'eap';
-grant all privileges on `eap-db` to 'eap'@'%';
--- script/sql/mysql/patch_yudao/system/oauth2_userkey.sql
+++
@@ -1,7 +0,0 @@
-
-
-ALTER TABLE `system_oauth2_refresh_token`
-    ADD COLUMN `user_key` varchar(50) NULL COMMENT 'account/username' AFTER `user_id`;
-
-ALTER TABLE `system_oauth2_access_token`
-    ADD COLUMN `user_key` varchar(50) NULL COMMENT 'account/username' AFTER `user_id`;
